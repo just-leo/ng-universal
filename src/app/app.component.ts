0356796ad6b929821cb7130a543c66411c24841f
Leo <l@codemotion.eu>
2018-01-28 18:22:13 +0200

    Start new project with bootstrap@3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [
-    '../../node_modules/bootstrap/dist/css/bootstrap.css',
-    '../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
-    './app.component.css'
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'It is an AppCmponent';
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { AlertComponent } from './alert/alert.component';
-import { AlertSuccessComponent } from './alert-success/alert-success.component';
-import { BindingTestComponent } from './binding-test/binding-test.component';
-import { ListTestComponent } from './list-test/list-test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    AlertComponent,
-    AlertSuccessComponent,
-    BindingTestComponent,
-    ListTestComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
