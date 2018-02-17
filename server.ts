8d1f0039f6eddcae329b285f44165e385c04116f
Leo <l@codemotion.eu>
2018-02-17 21:12:45 +0200

    ServerSideRendering !

---
+++ server.ts
@@ -0,0 +1,54 @@
+/**
+ * Created by leo on 15.02.18.
+ */
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import { platformServer, renderModuleFactory } from '@angular/platform-server'
+import { enableProdMode } from '@angular/core'
+import * as express from 'express';
+// Express Engine
+import { ngExpressEngine } from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+enableProdMode();
+
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'dist');
+
+// * NOTE :: leave this as require() since this file is built Dynamically from webpack
+// Get AppServerModule from build
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
+
+const app = express();
+
+app.engine('html', ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
+}));
+
+app.set('view engine', 'html');
+app.set('views', join(DIST_FOLDER, 'browser'));
+
+// TODO: implement data requests securely
+app.get('/api/*', (req, res) => {
+  res.status(404).send('data requests are not supported');
+});
+
+// Server static files from /browser
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+  res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
+});
+
+// Start up the Node server
+app.listen(PORT, () => {
+  console.log(`Node server listening on http://localhost:${PORT}`);
+});
